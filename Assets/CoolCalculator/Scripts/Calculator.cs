d31a32159f989b0803087de83db4f0f6528c6254
Samdroid <samueloyekoya12@gmail.com>
2023-01-15 14:27:15 +0100

    Project Setup
    All Project Files

---
+++ Assets/CoolCalculator/Scripts/Calculator.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Calculator : MonoBehaviour
+{
+    #region Fields
+    public TextMeshProUGUI InputText;
+    private float _result;
+    private float _input;
+    private float _input2;
+    private string _operation;
+    #endregion Fields
+
+    #region Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void ClickNumber(int val){
+        Debug.Log($" check val: {val}");
+        InputText.text = $"{val}";
+        if(_input == 0){
+            _input = val; 
+        } else {
+            _input2 = val;
+        }
+    }
+
+    public void ClickOperation(string val){
+        Debug.Log($" ClickOperation val: {val}");
+        _operation = val;
+    }
+
+    public void ClickEqual(string val){
+        Debug.Log($" ClickEqual val: {val}");
+        if (_input !=0 && _input2 !=0 && !string.IsNullOrEmpty(_operation)) {
+            switch (_operation)
+            {
+                case "+":
+                     _result = _input + _input2;
+                     break;
+                case "-":
+                     _result = _input - _input2;
+                    break;
+                case "*":
+                     _result = _input * _input2;
+                     break;
+                case "/":
+                     _result = _input / _input2;
+                     break;           
+            }
+            InputText.SetText(_result.ToString());
+            ClearInput();
+        }
+    }
+
+    public void ClickPeriod(string val){
+        Debug.Log($" ClickPeriod val: {val}");
+    }
+
+    public void ClearInput(){
+        _input = 0;
+        _input2 = 0;
+    }
+    #endregion Methods
+
+    #region Events
+    #endregion Events 
+
+}
